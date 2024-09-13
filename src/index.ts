e2c2975435e346eaf607918db1c0d5626d45c7a5
hbybyyang <2451759073@qq.com>
2024-09-13 10:07:05 +0800

    发布

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export function add(a: number, b: number): number {
-  return a + b
+import { Kysely } from 'kysely'
+import { z } from 'zod'
+import { 插件 } from '@lsby/net-core'
+
+const Kysely插件Zod = <DB>(): z.ZodObject<{ kysely: z.ZodType<Kysely<DB>> }> =>
+  z.object({
+    kysely: z.custom<Kysely<DB>>((instance) => instance instanceof Kysely),
+  })
+
+export class Kysely插件<DB> extends 插件<z.ZodObject<{ kysely: z.ZodType<Kysely<DB>> }>> {
+  constructor(kysely: Kysely<DB>) {
+    super(Kysely插件Zod(), async (_request, _response) => {
+      return { kysely }
+    })
+  }
 }
