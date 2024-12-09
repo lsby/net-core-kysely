a605aa0e526188ebed1de63a1e2d772531f04599
hbybyyang <2451759073@qq.com>
2024-12-10 04:15:10 +0800

    允许自定义注入名称

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import { Kysely } from 'kysely'
 import { z } from 'zod'
 import { 插件 } from '@lsby/net-core'
 
-const Kysely插件Zod = <DB>(): z.ZodObject<{ kysely: z.ZodType<Kysely<DB>> }> =>
-  z.object({
-    kysely: z.custom<Kysely<DB>>((instance) => instance instanceof Kysely),
-  })
+const Kysely插件Zod = <NAME extends string, DB>(name: NAME): z.ZodObject<{ [key in NAME]: z.ZodType<Kysely<DB>> }> => {
+  return z.object({
+    [name]: z.custom<Kysely<DB>>((instance) => instance instanceof Kysely),
+  } as { [key in NAME]: z.ZodType<Kysely<DB>> })
+}
 
-export class Kysely插件<DB> extends 插件<z.ZodObject<{ kysely: z.ZodType<Kysely<DB>> }>> {
-  constructor(kysely: Kysely<DB>) {
-    super(Kysely插件Zod(), async (_request, _response) => {
-      return { kysely }
+export class Kysely插件<NAME extends string, DB> extends 插件<z.ZodObject<{ [key in NAME]: z.ZodType<Kysely<DB>> }>> {
+  constructor(name: NAME, kysely: Kysely<DB>) {
+    super(Kysely插件Zod(name), async (_request, _response) => {
+      return { [name]: kysely } as any
     })
   }
 }
