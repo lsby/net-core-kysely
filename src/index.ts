160c48b447a1e65420b1322f05e41e4f126cb9f6
hbybyyang <2451759073@qq.com>
2025-05-16 07:16:33 +0800

    使用自定义句柄

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { Kysely } from 'kysely'
 import { z } from 'zod'
 import { 插件 } from '@lsby/net-core'
+import { Kysely管理器 } from '@lsby/ts-kysely'
 
-const Kysely插件Zod = <NAME extends string, DB>(name: NAME): z.ZodObject<{ [key in NAME]: z.ZodType<Kysely<DB>> }> => {
+const Kysely插件Zod = <NAME extends string, DB>(
+  name: NAME,
+): z.ZodObject<{ [key in NAME]: z.ZodType<Kysely管理器<DB>> }> => {
   return z.object({
-    [name]: z.custom<Kysely<DB>>((instance) => instance instanceof Kysely),
-  } as { [key in NAME]: z.ZodType<Kysely<DB>> })
+    [name]: z.custom<Kysely管理器<DB>>((instance) => instance instanceof Kysely管理器),
+  } as { [key in NAME]: z.ZodType<Kysely管理器<DB>> })
 }
 
-export class Kysely插件<NAME extends string, DB> extends 插件<z.ZodObject<{ [key in NAME]: z.ZodType<Kysely<DB>> }>> {
-  constructor(name: NAME, kysely: Kysely<DB>) {
+export class Kysely插件<NAME extends string, DB> extends 插件<
+  z.ZodObject<{ [key in NAME]: z.ZodType<Kysely管理器<DB>> }>
+> {
+  constructor(name: NAME, kysely: Kysely管理器<DB>) {
     super(Kysely插件Zod(name), async (_request, _response) => {
       return { [name]: kysely } as any
     })
