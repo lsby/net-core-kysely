c593dd637c4572fc83c406b3e7f53beec51a79a5
lsby <2451759073@qq.com>
2026-01-12 10:15:02 +0800

    适配新框架

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'
 import { 插件 } from '@lsby/net-core'
+import { Right } from '@lsby/ts-fp-data'
 import { Kysely管理器 } from '@lsby/ts-kysely'
 
 const Kysely插件Zod = <NAME extends string, DB>(
@@ -10,12 +11,16 @@ const Kysely插件Zod = <NAME extends string, DB>(
   } as { [key in NAME]: z.ZodType<Kysely管理器<DB>> })
 }
 
+var 错误类型描述 = z.never()
+
 export class Kysely插件<NAME extends string, DB> extends 插件<
-  z.ZodObject<{ [key in NAME]: z.ZodType<Kysely管理器<DB>> }>
+  typeof 错误类型描述,
+  ReturnType<typeof Kysely插件Zod<NAME, DB>>
 > {
   constructor(name: NAME, kysely: Kysely管理器<DB>) {
-    super(Kysely插件Zod(name), async (_request, _response) => {
-      return { [name]: kysely } as any
+    var 正确类型描述 = Kysely插件Zod(name)
+    super(错误类型描述, 正确类型描述 as any, async (_request, _response) => {
+      return new Right({ [name]: kysely }) as any
     })
   }
 }
